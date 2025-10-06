d0dd4fae1bfbffea3a5a01f10011ea035d33ddde
Junui Hong <jha357@sfu.ca>
2025-10-06 15:12:08 -0700

    tailwind  css installed

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <div className="flex justify-left pl-10 pt-5 pb-1 items-center bg-amber-100 text-blue-700 antialiased font-mono text-4xl">
+      This is Header
+    </div>
+  )
+}
