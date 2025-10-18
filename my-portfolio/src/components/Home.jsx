3641b3072ba27e466d1c3ca206ef99ed858b39b3
junuiui <jhong980821@outlook.com>
2025-10-17 19:44:43 -0700

    home (icon, text) added

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -1,26 +1,47 @@
 import Typewriter from 'typewriter-effect';
+import devPic from '../../public/devPic.png'
+
+// Icons
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-green-950">
-            {/* 프로필 이미지 */}
+            {/* profile Image  */ }
             <img
-                src="/my-photo.jpg"
+                src={ devPic }
                 alt="My profile"
-                className="w-40 h-40 rounded-full shadow-lg mb-6"
+                className="w-48 h-48 rounded-full shadow-lg mb-6"
             />
 
-            {/* 이름 */}
+            {/* name */ }
             <Typewriter
-                options={{
-                    strings: ["Hi, I am Junui Hong", "Software Developer/Engineer"],
+                options={ {
+                    strings: ["Hi, I am Junui Hong", "Software Developer/Engineer", "New Grad from Simon Fraser University"],
                     autoStart: true,
                     loop: true,
-                    pauseFor: 1500,
+                    cursor: '\u200B',
+                    delay: 50,
+                    deleteSpeed: 5,
                     wrapperClassName: "text-2xl md:text-2xl font-bold text-gray-800 dark:text-white tracking-wide",
                     cursorClassName: "text-2xl md:text-2xl font-bold text-gray-800 dark:text-white",
-                }}
+                } }
             />
+            {/* social links */ }
+            <div className="flex gap-6 mt-8">
+                <a href="https://github.com/junuiui" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="w-8 h-8 text-white hover:scale-200 transition-transform" />
+                </a>
+                <a href="https://linkedin.com/in/junui-hong-338487280/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="w-8 h-8 text-white hover:scale-200 transition-transform" />
+                </a>
+                <a href="https://instagram.com/junuiui" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className="w-8 h-8 text-white hover:scale-200 transition-transform" />
+                </a>
+            </div>
         </div>
     );
 }
