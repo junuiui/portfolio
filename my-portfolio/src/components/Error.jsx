d62c1b01c315d7427479c15fe9d26bf7b59327b9
junuiui <jhong980821@outlook.com>
2025-11-07 19:54:54 -0800

    any key press added

--- my-portfolio/src/components/Error.jsx
+++ my-portfolio/src/components/Error.jsx
@@ -1,15 +1,31 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-export default function Error() {
+export default function NotFound() {
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleKeyPress = () => navigate("/");
+    window.addEventListener("keydown", handleKeyPress);
+    console.log(handleKeyPress)
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, [navigate]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#100d74] text-white text-center px-6">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl mb-6">Oops! The page you're looking for doesn't exist.</p>
+
+      <h1 className="text-8xl font-bold mb-4 text-white/80">404</h1>
+      <div className="text-xl md:text-2xl font-semibold mb-6">
+        This page doesn't seem to exist.<br />
+        Press ANY key or press Home Button to cancel and return to Home.
+
+      </div>
+
+      {/* Home Button to return */ }
       <button
-        onClick={() => navigate("/")}
-        className="bg-white text-[#100d74] font-bold py-2 px-6 rounded hover:bg-gray-200 transition"
+        onClick={ () => navigate("/") }
+        className="bg-white text-[#100d74] font-bold py-2 px-6 rounded-lg hover:bg-gray-200 transition"
       >
         Go Home
       </button>
