2b4e62b796bc23f21136adc819937e293bf9b4fe
junuiui <jhong980821@outlook.com>
2025-11-18 16:33:25 -0800

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/junuihong-portfolio'
+    plugins: [react()],
+    base: process.env.NODE_ENV === 'production'
+        ? process.env.VITE_BASE_PATH || '/junuihong-portfolio'
+        : '/'
 })
